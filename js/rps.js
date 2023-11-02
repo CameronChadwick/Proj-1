764c318c758c342f5551510a19b5c1f4e7b5f202
CameronChadwick <cameronchad25@gmail.com>
2023-11-02 12:42:23 -0400

    rps nearly finished

--- js/rps.js
+++ js/rps.js
@@ -1,3 +1,4 @@
+// set the constant elements
 const rockButton = document.getElementById('rock')
 const paperButton = document.getElementById('paper')
 const scissorsButton = document.getElementById('scissors')
@@ -5,19 +6,46 @@ const result = document.querySelector('.result')
 const userImg = document.querySelector('.user-img')
 const compImg = document.querySelector('.comp-img')
 
-function rockBtnClick() {
-    result.innerText = 'rock clicked'
+const choices = ['rock', 'paper', 'scissors']
+let comp
+// // functions to change the result lines text
+// function rockBtnClick() {
+//     result.innerText = 'rock clicked'
+// }
+
+// function paperBtnClick() {
+//     result.innerText = 'paper clicked'
+// }
+
+// function scissorsBtnClick() {
+//     result.innerText = 'scissors clicked'
+// }
+
+
+// listen for events on the page
+// result.style.color = "red" ---> styling in js function using .style
+rockButton.addEventListener('click', () => {
+    userImg.src = "../img/" + rockButton.id + ".png"
+    compChoice()
+    winLose(rockButton.id)
+})
+paperButton.addEventListener('click', () => {
+    userImg.src = "../img/" + paperButton.id + ".png"
+    compChoice()
+    winLose(paperButton.id)
+})
+scissorsButton.addEventListener('click', () => {
+    userImg.src = "../img/" + scissorsButton.id + ".png"
+    compChoice()
+    winLose(scissorsButton.id)
+})
+
+function compChoice() {
+    const randNum = Math.floor(Math.random()*3)
+    comp = choices[randNum]
+    compImg.src = "../img/" + comp + ".png"
 }
 
-function paperBtnClick() {
-    result.innerText = 'paper clicked'
+function winLose(userChoice) {
+    console.log(comp, userChoice)
 }
-
-function scissorsBtnClick() {
-    result.innerText = 'scissors clicked'
-}
-
-
-rockButton.addEventListener('click', rockBtnClick)
-paperButton.addEventListener('click', paperBtnClick)
-scissorsButton.addEventListener('click', scissorsBtnClick)
