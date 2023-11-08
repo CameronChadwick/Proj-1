861cd1d15ab24303fadd389c45e09d93dc56bdf9
CameronChadwick <cameronchad25@gmail.com>
2023-11-08 12:27:10 -0500

    Bo5 Finisheds

--- js/rps.js
+++ js/rps.js
@@ -3,6 +3,7 @@ const rockButton = document.getElementById('rock')
 const paperButton = document.getElementById('paper')
 const scissorsButton = document.getElementById('scissors')
 const resetButton = document.getElementById('reset')
+const bestOfButton = document.getElementById('best-of')
 const result = document.querySelector('.result')
 const userImg = document.querySelector('.user-img')
 const compImg = document.querySelector('.comp-img')
@@ -10,6 +11,7 @@ const userScoreText = document.querySelector('.user-score')
 const compScoreText = document.querySelector('.comp-score')
 
 const choices = ['rock', 'paper', 'scissors']
+let ifBestOf = 0
 let comp
 let userScore = 0
 let compScore = 0
@@ -27,23 +29,67 @@ let compScore = 0
 //     result.innerText = 'scissors clicked'
 // }
 
-
 // listen for events on the page
 // result.style.color = "red" ---> styling in js function using .style
 rockButton.addEventListener('click', () => {
-    userImg.src = "../img/" + rockButton.id + ".png"
-    compChoice()
-    winLose(rockButton.id)
+    console.log(ifBestOf)
+    if (ifBestOf == 2) {
+        userScore = userScore
+        compScore = compScore
+        result.innerText = 'Reset Game'
+    }
+    else {
+        userImg.src = "../img/" + rockButton.id + ".png"
+        if (ifBestOf == 1) {
+            compChoice()
+            winLose(rockButton.id)
+            bestOf()
+        }
+        else {
+            compChoice()
+            winLose(rockButton.id)
+        }
+    }
 })
 paperButton.addEventListener('click', () => {
-    userImg.src = "../img/" + paperButton.id + ".png"
-    compChoice()
-    winLose(paperButton.id)
+    console.log(ifBestOf)
+    if (ifBestOf == 2) {
+        userScore = userScore
+        compScore = compScore
+        result.innerText = 'Reset Game'
+    }
+    else {
+        userImg.src = "../img/" + paperButton.id + ".png"
+        if (ifBestOf == 1) {
+            compChoice()
+            winLose(paperButton.id)
+            bestOf()
+        }
+        else {
+            compChoice()
+            winLose(paperButton.id)
+        }
+    }
 })
 scissorsButton.addEventListener('click', () => {
-    userImg.src = "../img/" + scissorsButton.id + ".png"
-    compChoice()
-    winLose(scissorsButton.id)
+    console.log(ifBestOf)
+    if (ifBestOf == 2) {
+        userScore = userScore
+        compScore = compScore
+        result.innerText = 'Reset Game'
+    }
+    else {
+        userImg.src = "../img/" + scissorsButton.id + ".png"
+        if (ifBestOf == 1) {
+            compChoice()
+            winLose(scissorsButton.id)
+            bestOf()
+        }
+        else {
+            compChoice()
+            winLose(scissorsButton.id)
+        }
+    }
 })
 
 resetButton.addEventListener('click', () => {
@@ -54,6 +100,18 @@ resetButton.addEventListener('click', () => {
     result.innerText = 'Start Game'
     userScoreText.innerText = userScore.toString()
     compScoreText.innerText = compScore.toString()
+    ifBestOf = 0
+})
+
+bestOfButton.addEventListener('click', () => {
+    userScore = 0
+    compScore = 0
+    userImg.src = "../img/rps4.png"
+    compImg.src = "../img/rps4.png"
+    result.innerText = 'Start Best of 5'
+    userScoreText.innerText = userScore.toString()
+    compScoreText.innerText = compScore.toString()
+    ifBestOf = 1
 })
 
 function compChoice() {
@@ -62,29 +120,40 @@ function compChoice() {
     compImg.src = "../img/" + comp + ".png"
 }
 
+function bestOf() {
+    if (userScore == 3) {
+        result.innerText = 'User Wins Best of 5'
+        ifBestOf = 2
+    }
+    if (compScore == 3) {
+        result.innerText = 'Computer Wins Best of 5'
+        ifBestOf = 2
+    }
+}
+
 function winLose(userChoice) {
     if (userChoice == "rock" && comp == "paper") {
-        result.innerText = 'Computer Wins'
+        result.innerText = 'Computer Score +1'
         compScore = compScore + 1
         compScoreText.innerText = compScore.toString()
     } else if (userChoice == "paper" && comp == "scissors") {
-        result.innerText = 'Computer Wins'
+        result.innerText = 'Computer Score +1'
         compScore = compScore + 1
         compScoreText.innerText = compScore.toString()
     } else if (userChoice == "scissors" && comp == "rock") {
-        result.innerText = 'Computer Wins'
+        result.innerText = 'Computer Score +1'
         compScore = compScore + 1
         compScoreText.innerText = compScore.toString()
     } else if (comp == "rock" && userChoice == "paper") {
-        result.innerText = 'User Wins'
+        result.innerText = 'User Score +1'
         userScore = userScore + 1
         userScoreText.innerText = userScore.toString()
     } else if (comp == "paper" && userChoice == "scissors") {
-        result.innerText = 'User Wins'
+        result.innerText = 'User Score +1'
         userScore = userScore + 1
         userScoreText.innerText = userScore.toString()
     } else if (comp == "scissors" && userChoice == "rock") {
-        result.innerText = 'User Wins'
+        result.innerText = 'User Score +1'
         userScore = userScore + 1
         userScoreText.innerText = userScore.toString()
     } else if (comp == "rock" && userChoice == "rock") {
